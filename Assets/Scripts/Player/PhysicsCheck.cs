18f7ce60b5a432ce41412f99327461dd1e60bdfb
DUOJING98 <duojing98@gmail.com>
2025-02-13 21:58:28 +0900

    添加背景

--- Assets/Scripts/Player/PhysicsCheck.cs
+++ Assets/Scripts/Player/PhysicsCheck.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 
 public class PhysicsCheck : MonoBehaviour
 {
+    [Header("Check")]
     public bool isGround;
+    public Vector2 bottomOffset;
     public float checkRaduis;
     public LayerMask groundLayer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -19,6 +21,12 @@ public class PhysicsCheck : MonoBehaviour
 
     public void Check()
     {
-        isGround = Physics2D.OverlapCircle(transform.position, checkRaduis, groundLayer);
+        isGround = Physics2D.OverlapCircle((Vector2)transform.position+bottomOffset, checkRaduis, groundLayer);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere((Vector2)transform.position + bottomOffset, checkRaduis);
     }
 }
